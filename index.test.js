4bdecb341514ca64666455c78ca203c82dbb4ab4
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-24 08:47:03 -0400

    Adding in the mocha framework

---
+++ index.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const { forEach, map } = require('./index');
+
+it('the forEach function', () => {
+
+  let sum = 0;
+  forEach([1, 2, 3], value => {
+    sum += value;
+  });
+
+  assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
+  
+});
+
+it('the map function', () => {
+  const result = map([1, 2, 3], value => {
+    return value * 2;
+  });
+
+  assert.deepStrictEqual(result, [2,4,6]);
+  // assert.strictEqual(result[0], 2);
+  // assert.strictEqual(result[1], 4);
+  // assert.strictEqual(result[2], 6);
+  
+});
+
